f0ee3cb32b5a29d793bf2b887e90848dd37197d9
P217F <vuongiavinhdieu2k9@gmail.com>
2025-10-22 16:55:06 +0700

    #3: commit

--- src/a.c
+++ src/a.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <wayland-server.h>
+
 int main() {
+    struct wl_display *display = wl_display_create();
+    if (!display) {
+        fprintf(stderr, "Error: cannot create wayland server\n");
+        return 1;
+    }
+    fprintf(stdout, "Running wayland server atomic\n");
+    wl_display_run(display);
+    wl_display_destroy(display);
+
+    // Exit program
     return 0;
 }
